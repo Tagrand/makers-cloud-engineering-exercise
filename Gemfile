128b93c1646a6589b2f313a1576e2b792f9ff13f
Tom Grand <thomas.a.grand@gmail.com>
2018-03-23 15:43:26 +0000

    Add database cleaner I added this after getting the tests to work on the database. This way, I know for certain that if any errors arose, it was with way I set up the cleaner, not the actual database structure.

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sinatra'
 group :test do
   gem 'capybara'
   gem 'rspec'
+  gem 'database_cleaner'
   gem 'rubocop', '~> 0.54.0'
   gem 'simplecov', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require './app.rb'
 require 'capybara/rspec'
 require 'simplecov'
 require './models/rating'
+require 'database_cleaner'
 
 Capybara.app = FTFeedback
 SimpleCov.start
@@ -17,4 +18,19 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
 end
