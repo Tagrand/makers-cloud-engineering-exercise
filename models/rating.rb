14aca69fb5290a3fc92e864c5c086d2b6d5b414a
Tom Grand <thomas.a.grand@gmail.com>
2018-03-23 17:56:06 +0000

    Add score_messages I moved the logic of formatting the score error messages to a method in the model. In the longer term, if the messages got more complex I may well move this into a separate class. However, as its relatively simple I've kept it in the same class to start with to avoid abstracting too early.

--- models/rating.rb
+++ models/rating.rb
@@ -8,6 +8,10 @@ class Rating
 
   property :id, Serial
   property :score, Integer, required: true
+
+  def score_messages
+    self.errors[:score].first
+  end
 end
 
 DataMapper.setup(:default, "mysql://ft@localhost/ft_#{ENV['RACK_ENV']}")
--- spec/rating_spec.rb
+++ spec/rating_spec.rb
@@ -8,4 +8,11 @@ describe Rating do
   it 'should have a score' do
     expect { Rating.create }.not_to change(Rating, :count)
   end
+
+  describe '#score_messages' do
+    it 'returns error message when score is nil' do
+      result = Rating.create
+      expect(result.score_messages).to eq('Score must not be blank')
+    end
+  end
 end
