cac33e9a11b63b9a7515a38b0b70231946d1eb4d
Tom Grand <thomas.a.grand@gmail.com>
2018-03-26 09:01:55 +0100

    Change all refernces of review to rating
    To make sure the language in the project is consistent with the brief

--- app.rb
+++ app.rb
@@ -12,28 +12,28 @@ class FTFeedback < Sinatra::Base
   register Sinatra::Flash
 
   get '/' do
-    redirect '/reviews/new'
+    redirect '/ratings/new'
   end
 
-  get '/reviews/new' do
+  get '/ratings/new' do
     @rating_range = (Rating::MIN_SCORE..Rating::MAX_SCORE)
     erb :index
   end
 
-  post '/reviews' do
+  post '/ratings' do
     score = params[:rating].to_i unless params[:rating].nil?
     result = Rating.new(score: score)
     if result.save
       flash.next[:message] = 'Thanks for your feedback!'
-      redirect '/reviews'
+      redirect '/ratings'
     else
       flash.next[:error] = result.score_messages
-      redirect 'reviews/new'
+      redirect 'ratings/new'
     end
   end
 
-  get '/reviews' do
-    @reviews = Rating.all
-    erb :reviews
+  get '/ratings' do
+    @ratings = Rating.all
+    erb :ratings
   end
 end
